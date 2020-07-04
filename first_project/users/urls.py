f6c4a172616c402dee41bd075ca96166586af5dc
Ricardo Quijas <ricardo.quijass@gmail.com>
2020-07-03 20:46:08 -0500

    Actualización 3 de julio 2020

--- first_project/users/urls.py
+++ first_project/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import GetUsers
+from .views import GetUsers, GetUser, CreateUser
 
 app_name='users'
 urlpatterns = [
-    path('', GetUsers, name='list')
+    path('', GetUsers, name='list'),
+    path('<int:id>/', GetUser, name='detail'),
+    path('create/', CreateUser.as_view(), name='create')
 ]
--- first_project/users/views.py
+++ first_project/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django import views
 from django.contrib.auth.models import User
+from .forms import UserForm, ProfileForm 
 
 def GetUsers(request):
     users = User.objects.filter(is_superuser=False)
@@ -8,4 +10,37 @@ def GetUsers(request):
         'users': users
     }
     return render(request, template_name, context)
+
+def GetUser(request, id):
+    user = User.objects.get(id=id)
+    print(user.profile.image)
+    template_name = "users/detail.html"
+    context = {
+        'user': user 
+    }
+    return render(request, template_name, context)
+    
+    
+    
+class CreateUser(views.View):
+    def get(self, request):  
+        user_form = UserForm()
+        profile_form = ProfileForm()
+        template_name = 'users/form.html'
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
+        return render(request, template_name, context)
+    def post(self, request):
+        user_form = UserForm(request.POST)
+        profile_form = ProfileForm(request.POST, request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save(commit=False)
+            user.set_password(user_form.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            
 
